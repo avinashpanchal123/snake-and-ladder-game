df9f4004b7974bc7087b19086f98acb4d287b737
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-03-16 23:28:53 +0530

    created function for handling player movements from 94th to 100th cell
    created function such that player does not outside the board and notified when he/she won the game

--- javascript/player.js
+++ javascript/player.js
@@ -13,9 +13,9 @@ player1.onclick = () => {
 };
 
 function Decision() {
-    let d1 = Math.floor(Math.random() * 6) +1;
-    // let d1 = 1;
-    
+  // let d1 = Math.floor(Math.random() * 6) + 1;
+  let d1 = 1;
+
   let d2 = Math.floor(Math.random() * 6) + 1;
   let timer = setTimeout(() => {
     dice1.src = "../images/dice/dice" + d1 + ".png";
@@ -43,7 +43,7 @@ var positionY = 0;
 function handlePosition(moves_right) {
   for (let i = 0; i < moves_right; i++) {
     positionX = positionX + 3.5;
-    if (positionX < 35) {
+    if (positionX < 35 && positionX >= 0 && positionY <= 0 && positionY > -35) {
       p1.style.transform = `translate(${positionX}rem, ${positionY}rem)`;
     } else {
       positionX = 31.5;
@@ -54,66 +54,57 @@ function handlePosition(moves_right) {
       break;
     }
   }
-  //  alert([positionX, positionY]);
+  // alert([positionX, positionY]);
 
   if (positionY == 0 && positionX == 10.5) {
-    small_ladder();
-  } 
-  else
-   if (positionY == -7 && positionX == 24.5) {
-    big_ladder();
+    // small_ladder();
+  } else if (positionY == -7 && positionX == 24.5) {
+    // big_ladder();
+  } else if (positionX == 21 && positionY == -21) {
+    // medium_ladder();
+  } else if (positionX == 0 && positionY == -28) {
+    // leftSide_snake();
   }
-  else
-   if( positionX == 21 && positionY == -21){
-    medium_ladder()
-  }
-  else
-   if( positionX == 0 && positionY == -28){
-    leftSide_snake()
-  }
-
- }
+}
 
 function handlePosition1(moves_left) {
-  for (let i = 0; i < moves_left; i++) {
-    if (positionX > 0) {
-      positionX = positionX - 3.5;
-      p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
-    } else {
-      flag = true;
-      positionY = positionY - 3.5;
-      p1.style.transform = `translateY(${positionY}rem)`;
-      positionX = 0;
-      handlePosition(moves_left - i - 1);
-      break;
+  if (positionX <= 21 && positionY == -31.5) {
+    go_to_Home(moves_left);
+  } else {
+    for (let i = 0; i < moves_left; i++) {
+      if (
+        positionX < 35 &&
+        positionX > 0 &&
+        positionY <= 0 &&
+        positionY > -35
+      ) {
+        positionX = positionX - 3.5;
+        p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
+      } else {
+        flag = true;
+        positionY = positionY - 3.5;
+        p1.style.transform = `translateY(${positionY}rem)`;
+        positionX = 0;
+        handlePosition(moves_left - i - 1);
+        break;
+      }
     }
   }
 
-  // alert([positionX, positionY])
+  // alert([positionX, positionY]);
 
-
-  if( positionX == 7 && positionY == -10.5){
-    medium_ladder01()
-  }
-  else 
-  if( positionX == 31.5 && positionY == -10.5){
-    small_ladder01()
-  }
-  else
-   if( positionX == 10.5 && positionY == -10.5){
-    medium_snake()
-  }
-  else
-   if( positionX == 31.5 && positionY == -24.5){
-      long_snake()
-  }
-  else 
-  if( positionX == 7 && positionY == -31.5){
-      soLong_snake()
-  }
-  else
-   if( positionX == 17.5 && positionY == -24.5){
-    medium2_snake()
+  if (positionX == 7 && positionY == -10.5) {
+    // medium_ladder01();
+  } else if (positionX == 31.5 && positionY == -10.5) {
+    // small_ladder01();
+  } else if (positionX == 10.5 && positionY == -10.5) {
+    // medium_snake();
+  } else if (positionX == 31.5 && positionY == -24.5) {
+    // long_snake();
+  } else if (positionX == 7 && positionY == -31.5) {
+    // soLong_snake();
+  } else if (positionX == 17.5 && positionY == -24.5) {
+    // medium2_snake();
   }
 }
 
@@ -131,67 +122,77 @@ function big_ladder() {
   flag = true;
 }
 
-
-function medium_ladder01(){
-    // alert("I am called")
-    positionX = positionX - 7;
-    positionY = positionY - 10.5;
-    p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
-    flag = true;
+function medium_ladder01() {
+  // alert("I am called")
+  positionX = positionX - 7;
+  positionY = positionY - 10.5;
+  p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
+  flag = true;
 }
 
-
-function medium_ladder(){
-    positionX = positionX + 7;
-    positionY = positionY - 10.5;
-    p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
-    flag = false;
+function medium_ladder() {
+  positionX = positionX + 7;
+  positionY = positionY - 10.5;
+  p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
+  flag = false;
 }
 
-
-function small_ladder01(){
-    positionX = positionX - 3.5;
-    positionY = positionY - 7;
-    p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
-    flag = false;
+function small_ladder01() {
+  positionX = positionX - 3.5;
+  positionY = positionY - 7;
+  p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
+  flag = false;
 }
 
-
-function medium_snake(){
-    positionX = positionX -3.5;
-    positionY = positionY + 10.5;
-    p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
-    flag = true;
+function medium_snake() {
+  positionX = positionX - 3.5;
+  positionY = positionY + 10.5;
+  p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
+  flag = true;
 }
 
-function long_snake(){
-    alert("I am called")
-    positionX = positionX - 14;
-    positionY = positionY + 17.5;
-    p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
-    flag = true;
+function long_snake() {
+  alert("I am called");
+  positionX = positionX - 14;
+  positionY = positionY + 17.5;
+  p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
+  flag = true;
 }
 
+function soLong_snake() {
+  positionX = positionX - 7;
+  positionY = positionY + 28;
+  p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
+  flag = false;
+}
 
-function soLong_snake(){
+function leftSide_snake() {
+  positionX = positionX + 7;
+  positionY = positionY + 10.5;
+  p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
+  flag = false;
+}
+
+function medium2_snake() {
+  positionX = positionX + 7;
+  positionY = positionY + 14;
+  p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
+  flag = false;
+}
+
+function go_to_Home(moves_left) {
+ 
+  let move = moves_left * 3.5
+  positionX = positionX - move  
+  alert(positionX)
+
+  if( positionX  == 0){
+    
+    p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
+    alert("you win the game")
+  }
+  else if( positionX > 0) {
   
-    positionX = positionX - 7;
-    positionY = positionY + 28;
     p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
-    flag = false;
-}
-
-
-function leftSide_snake(){
-    positionX = positionX + 7;
-    positionY = positionY + 10.5;
-    p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
-    flag = false;
-}
-
-function medium2_snake(){
-    positionX = positionX + 7;
-    positionY = positionY + 14;
-    p1.style.transform = `translate(${positionX}rem,${positionY}rem)`;
-    flag = false;
+  }
 }
